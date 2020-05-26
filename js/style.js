d0d5d6c09e15c315d0d501cdd7bc0e5dee110813
pradeepsuthar <sutharpradeep081@gmail.com>
2020-05-26 12:02:11 +0530

    check indent

---
+++ js/style.js
@@ -0,0 +1,16 @@
+// owl Carousel
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items: 1,
+    loop: true,
+    margin: 0,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: false,
+});
+$('.play').on('click', function () {
+    owl.trigger('play.owl.autoplay', [1000])
+})
+$('.stop').on('click', function () {
+    owl.trigger('stop.owl.autoplay')
+})
